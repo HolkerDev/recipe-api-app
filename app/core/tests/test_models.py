4514aca50e2f52311356f87a6e8ff2984c40678c
HolkerDev <haron.dev@gmail.com>
2020-01-05 23:30:42 +0100

    Added upload image feature

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from .. import models
+from unittest.mock import patch
 
 
 def sample_user(email="test.random@mail.com", password="11111"):
@@ -64,3 +65,14 @@ class ModelTests(TestCase):
             price=5.00
         )
         self.assertEqual(str(recipe), recipe.title)
+
+    @patch('uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test that image is saved in the correct location"""
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        filepath = models.recipe_image_file_path(None, 'myname.jpg')
+
+        exp_path = f'uploads/recipe/{uuid}.jpg'
+        self.assertEqual(filepath, exp_path)
+
